fd31c9686c5149c6ddf7ad13d9f45a55f6b48b4e
Robert Clark <robert@nerfedmedia.com>
2024-12-26 23:42:11 -0800

    published_at sorted with nulls last

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,7 +1,8 @@
 class BlogPost < ApplicationRecord
   validates_presence_of :title, presence: true
   validates_presence_of :body, presence: true
-  scope :sorted, ->{ order(published_at: :desc, updated_at: :desc) }
+
+  scope :sorted, ->{ order(arel_table[published_at].desc.nulls_last).order(updated_at: :desc) }
   scope :draft, -> { where(published_at: :nil) }
   scope :published, -> { where("published_at<= ? ", Time.current) }
   scope :scheduled, -> { where("published_at > ?", Time.current) }
@@ -13,7 +14,9 @@ class BlogPost < ApplicationRecord
   def published?
     published_at? && published_at <= Time.current
   end
+
   def scheduled?
     published_at? && published_at > Time.current
   end
+
 end
